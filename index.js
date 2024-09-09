997e831a9308908ebfeef5db6bd2a411e3d83562
EllenaAmanda <ellenaamanda846@gmail.com>
2024-09-09 20:32:26 +0700

    done

--- index.js
+++ index.js
@@ -6,11 +6,16 @@
 
 let arrInput = [1,4,6,7]
 
+function arrayKaliReverse(arrInput){
+    let arrayInputNew = arrInput.map(item =>{
+        return item * 2
+    })
+    
+    return arrayInputNew.reverse()
+}
 
-
-let arrayInputNew = arrInput.map(item =>{
-    return item * 2
-})
-
-console.log(arrayInputNew.reverse())
+console.log(arrayKaliReverse(arrInput))
+console.log(arrayKaliReverse([7,4,2,1]))
+console.log(arrayKaliReverse([5]))
+console.log(arrayKaliReverse([1,3]))
 
