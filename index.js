799e63fde41859ce50cdfaf17b4dab0e16d128ee
EllenaAmanda <ellenaamanda846@gmail.com>
2024-09-09 20:18:53 +0700

    5 menit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+// input        // result
+// [1,4,6,7]       // [14,12,8,2]
+// [7,4,2,1]       // [2,4,8,14]
+// [5]             // [10]
+// [1,3]			//[6,2]
+
+let arrInput = [1,4,6,7]
+
+arrInput.map(item =>{
+    let hasil = item*2
+    console.log(hasil)
+})
